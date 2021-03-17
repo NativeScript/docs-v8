5944b9e39c40cf1da80af9a654a5f0b88f02ce37
Igor Randjelovic <rigor789@gmail.com>
2021-03-17 17:00:43 +0100

    chore: test flavor switcher
    unused for the moment

---
+++ test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        Selected: {{ selectedFlavor }}
+        
+        <div>
+            <select v-model="selectedFlavor">
+                <option v-for="flavor in flavors" :value="flavor.value" :key="flavor.value">{{ flavor.name }}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const flavors = [
+    { name: 'No Flavor', value: 'plain' },
+    { name: 'Angular', value: 'angular' },
+    { name: 'Vue', value: 'vue' },
+    { name: 'React', value: 'react' },
+    { name: 'Svelte', value: 'svelte' },
+]
+
+const selectedFlavor = ref(flavors[0].value)
+
+watch(selectedFlavor, () => {
+    document.body.className = `active-${selectedFlavor.value}`
+}, { immediate: true })
+</script>
