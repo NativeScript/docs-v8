c21107c2267866db52a8cfc9f63106231e02669e
Igor Randjelovic <rigor789@gmail.com>
2021-10-08 19:06:20 +0200

    fix: await promises then exit

--- scripts/plugin-scraper.mjs
+++ scripts/plugin-scraper.mjs
@@ -11,63 +11,72 @@ async function main() {
 			headless: true,
 		})
 
-		plugins.forEach(async (item) => {
-			item.children.forEach(async (plug) => {
-				try {
-					// have to be sure we go to the correct repo since payments and plugins are different
-					let baseUrl
-					if (item.text === 'Official Plugins') {
-						baseUrl =
-							'https://raw.githubusercontent.com/NativeScript/plugins/main/packages'
-					} else if (item.text === 'Payment Plugins') {
-						baseUrl =
-							'https://raw.githubusercontent.com/NativeScript/payments/main/packages'
-					}
+		const promises = []
 
-					// now do work
-					if (baseUrl) {
-						// fix the url for the plugin we're grabbing
-						const url = `${baseUrl}/${plug.link.replace(
-							'/plugins/',
-							''
-						)}/README.md`
-
-						log(`Fetching: ${url}`)
-
-						const page = await browser.newPage()
-
-						// Go to the plugin page and locate the README.md
-						const response = await page.goto(url, {
-							waitUntil: 'domcontentloaded',
-						})
-
-						// this is the README text :)
-						const data = await response.text()
-
-						const headerSnippet = [
-							`---`,
-							`title: ${plug.text}`,
-							`link: ${url}`,
-							`---`,
-						].join('\n')
-
-						// save the file
-						fse.outputFileSync(
-							`./${plug.link}.md`,
-							`${headerSnippet}\n\n${data.trim()}`
-						)
-						log(chalk.green(`File saved for plugin: ${plug.text}`))
-					}
-				} catch (error) {
-					log(chalk.red(error))
+		plugins.forEach((item) => {
+			item.children.forEach((plug) => {
+				// have to be sure we go to the correct repo since payments and plugins are different
+				let baseUrl
+				if (item.text === 'Official Plugins') {
+					baseUrl =
+						'https://raw.githubusercontent.com/NativeScript/plugins/main/packages'
+				} else if (item.text === 'Payment Plugins') {
+					baseUrl =
+						'https://raw.githubusercontent.com/NativeScript/payments/main/packages'
 				}
+
+				// now do work
+				if (!baseUrl) {
+					return
+				}
+				const fetchData = async () => {
+					// fix the url for the plugin we're grabbing
+					const url = `${baseUrl}/${plug.link.replace(
+						'/plugins/',
+						''
+					)}/README.md`
+
+					log(`Fetching: ${url}`)
+
+					const page = await browser.newPage()
+
+					// Go to the plugin page and locate the README.md
+					const response = await page.goto(url, {
+						waitUntil: 'domcontentloaded',
+					})
+
+					// this is the README text :)
+					const data = await response.text()
+
+					const headerSnippet = [
+						`---`,
+						`title: ${plug.text}`,
+						`link: ${url}`,
+						`---`,
+					].join('\n')
+
+					// save the file
+					fse.outputFileSync(
+						`./${plug.link}.md`,
+						`${headerSnippet}\n\n${data.trim()}`
+					)
+					log(chalk.green(`File saved for plugin: ${plug.text}`))
+				}
+
+				promises.push(
+					fetchData().catch((error) => {
+						log(chalk.red(error))
+					})
+				)
 			})
 		})
 
-		// process.exit()
+		await Promise.all(promises)
+		log(chalk.green('Done.'))
+		process.exit();
 	} catch (error) {
 		log(chalk.red(error))
-		process.exit()
+		process.exit();
 	}
 }
 
