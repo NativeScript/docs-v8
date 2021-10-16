298b6af619f65d9819285d087bcf9133436913ba
Janos Hrubos <33330538+janoshrubos@users.noreply.github.com>
2021-10-16 21:36:10 +0200

    feat(plugins): transform note styles (#52)
    * feat(plugins): transform note styles
    
    * chore: add comment
    
    * refactor: handle multiple lines with more container types

--- scripts/plugin-scraper.mjs
+++ scripts/plugin-scraper.mjs
@@ -46,7 +46,7 @@ async function main() {
 			// save the file
 			fse.outputFileSync(
 				`./${plugin.link}.md`,
-				`${headerSnippet}\n\n${repoSnippet}\n\n${data.trim()}`
+				`${headerSnippet}\n\n${repoSnippet}\n\n${transformNoteStyle(data)}`
 			)
 			log(chalk.green(`File saved for plugin: ${plugin.name}`))
 		}
@@ -78,6 +78,40 @@ async function main() {
 		log(chalk.red(error), error)
 		process.exit()
 	}
+}
+
+function transformNoteStyle(content) {
+	// transforms note styles for vitepress
+	const debug = false;
+	const NOTE_RE = /^>\s+\*{2}([^\*]+)\*{2}:?[^\S\r\n]+(.+(?:\n>.*)*)/gim;
+	let match_;
+	const containerTypeMap = {
+		'note': 'tip',
+		'warning': 'warning',
+	}
+		
+	while(match_ = NOTE_RE.exec(content)) {
+		let [match, title, cont] = match_;
+		
+		debug && console.log({
+			match, title, cont
+		})
+		
+		// normalize title
+		title = title.replace(/:/g, '');
+		
+		// replace "> " from multiline quotes
+		cont = cont.replace(/^>\s*/gim, '');
+		
+		// generate replacement
+		let containerType = containerTypeMap[title.toLowerCase()] ?? 'tip';
+		const replacement = `:::${containerType} ${title}\n\n${cont}\n\n:::`;
+		
+		// replace the match with the replacement
+		content = content.replace(match, replacement);
+	  }
+
+	return content;
 }
 
 main()
